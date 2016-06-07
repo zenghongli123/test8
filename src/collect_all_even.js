5dfdd23affa0fff45fd7ecdc6eb0a62d9d942c00
zenghongli123 <lalalalaxxx123@sina.com>
2016-06-07 21:56:38 +0800

    test8

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var arry=new Array();
+  var k=0;
+  for(var i=0;i<collection.length;i++)
+  {
+  	if(collection[i]%2==0)
+  	{
+  		arry[k]=collection[i];
+  		k++;
+  	}
+  }
+  return arry;
 }
 
 module.exports = collect_all_even;
